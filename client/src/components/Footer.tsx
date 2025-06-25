aac7a17ca5eeeb1daad49f94daa7e08452fc913b
Liangce Li <liliangce316@gmail.com>
2025-06-25 22:07:36 +0930

    Fix: fix style of footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-primary-700 text-center text-sm text-primary-100 py-4 fixed bottom-0 w-full">
+    <footer className="bg-primary-700 text-center text-sm text-primary-100 py-4 fixed left-0 bottom-0 w-full">
       © {new Date().getFullYear()} Proa Energy. All rights reserved.
     </footer>
   );
